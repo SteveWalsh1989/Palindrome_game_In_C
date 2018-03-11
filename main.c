def33a51043fd06fb54753c8d170a04954569157
SteveWalsh1989 <stevewalsh89@gmail.com>
2018-03-11 23:04:32 +0000

    Palindrome game
     Presents list of numbers to user and goal is to make it a palindrome
     User can use keyboard to move between numbers using a and d keys
     user can increment or decrement values in list using w and x keys
      Program will count number of goes it takes user to complete game
    
     Trying to implement the feature to work with the initialNumber
    to create the array of numbers for user to play with
     1 : Doesnt increase / decrease digits using w / x

---
+++ main.c
@@ -0,0 +1,37 @@
+
+#include "pal.h"
+
+/****************************
+ *
+ * C Programming Assignment 1 v2
+ *
+ * Steve Walsh
+ *
+ * R00151053
+ *
+ * 11/3/2018
+ *
+ * Palindrome game
+ *
+ * Presents list of numbers to user and goal is to make it a palindrome
+ * User can use keyboard to move between numbers using a and d keys
+ * user can increment or decrement values in list using w and x keys
+ * Program will count number of goes it takes user to complete game
+ *
+ *
+ *
+ *        Trying to implement the feature to work with the initialNumber
+ *        to create the array of numbers for user to play with
+ *        1 : Doesnt increase / decrease digits using w / x
+ *
+ *****************************/
+
+
+int main() {
+
+    int initialNumber  = 193643;      // stores integer to be converted into list
+
+    start_game(&initialNumber);        // launches game
+
+    return 0;
+}
